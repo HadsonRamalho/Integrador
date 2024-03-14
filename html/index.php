948e9317ca984dcbe1c5ccf4d36bc8e0fc9a4f49
HadsonRamalho <hadsonramalho@gmail.com>
2024-03-14 00:36:37 -0300

    Alterando a extensão do arquivo index.html para .php

---
+++ html/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title> Projeto Integrador </title>
+</head>
+
+<body>
+    <?php
+        echo "Práticas Profissionais Integradoras I";
+    ?>
+
+
+</body>
+
+</html>
