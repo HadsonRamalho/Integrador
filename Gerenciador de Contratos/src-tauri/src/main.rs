0cc92084ce599e3166e19bc2460254c5847529a8
HadsonRamalho <hadsonramalho@gmail.com>
2024-04-17 13:33:51 -0300

    Preparando e testando a stack

--- Gerenciador de Contratos/src-tauri/src/main.rs
+++ Gerenciador de Contratos/src-tauri/src/main.rs
@@ -4,12 +4,34 @@
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    let mut vet_usuarios:[Usuario;10] = Default::default();
+    let mut encontrado = false;
+    vet_usuarios[0].email = "user1@u.com".to_string();
+    vet_usuarios[1].email = "user2@u.com".to_string();
+    vet_usuarios[3].email = "user3@u.com".to_string();
+    let mut indice:u32 = 0;
+    for i in vet_usuarios.iter() {
+        indice +=1 ;
+        if i.email.eq_ignore_ascii_case(name.trim()) {
+            encontrado = true;            
+            break;
+        }
+    }
+    if(encontrado){
+        format!("E-mail {} encontrado!", name)
+    } else {
+        format!("E-mail {} não encontrado!", name)
+    }
+}
+
+#[derive(Default)]
+struct Usuario{
+    nome:String, email:String, senha:String, UID:u32
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
+       .invoke_handler(tauri::generate_handler![greet])
+       .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- Gerenciador de Contratos/src/App.jsx
+++ Gerenciador de Contratos/src/App.jsx
@@ -11,25 +11,10 @@ function App() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     setGreetMsg(await invoke("greet", { name }));
   }
-
+  
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
+      <p>[DEV | Back] Testando busca de email</p>
       <form
         className="row"
         onSubmit={(e) => {
@@ -40,9 +25,9 @@ function App() {
         <input
           id="greet-input"
           onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
+          placeholder="E-mail a ser procurado..."
         />
-        <button type="submit">Greet</button>
+        <button type="submit">Procurar</button>
       </form>
 
       <p>{greetMsg}</p>
