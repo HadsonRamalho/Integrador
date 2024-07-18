98767aeb489d2ef7269fc8240d999589d8de8f6e
HadsonRamalho <hadsonramalho@gmail.com>
2024-07-18 14:55:03 -0300

    Aprimorando consultas no banco de dados, parâmetros de funções e lógica de verificação

--- Gerenciador de Contratos/src/menu.jsx
+++ Gerenciador de Contratos/src/menu.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import CPDF from './pdf_call';
 import './styles.css';
 
+localStorage.removeItem('token');
+
 const rootElement = document.getElementById('pdf');
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
