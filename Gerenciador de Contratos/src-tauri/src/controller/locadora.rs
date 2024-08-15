d12c3cd63bc1303f19ec2d931c1cc6e7508f1647
HadsonRamalho <hadsonramalho@gmail.com>
2024-08-15 16:36:25 -0300

    Adicionando campo cnpj ao construtor do json da Locadora

--- Gerenciador de Contratos/src-tauri/src/controller/locadora.rs
+++ Gerenciador de Contratos/src-tauri/src/controller/locadora.rs
@@ -2,12 +2,36 @@ use mysql_async::prelude::Queryable;
 
 use crate::controller;
 
+/// Função para criar uma estrutura de dados para uma locadora.
+///
+/// # Parâmetros
+/// - idendereco: Identificador do endereço associado à locadora.
+/// - cnpj: Cadastro Nacional da Pessoa Jurídica da locadora.
+/// - numerocontabanco: Número da conta bancária da locadora.
+/// - numeroagenciabanco: Número da agência bancária da locadora.
+/// - nomebanco: Nome do banco onde a locadora possui conta.
+///
+/// # Processo
+/// 1. Criptografa o CNPJ da locadora para criar um identificador único.
+/// 2. Cria um objeto JSON com os seguintes dados:
+///    - `idendereco`: Identificador do endereço.
+///    - `idlocadora`: Identificador único criptografado para a locadora.
+///    - `cnpj`: CNPJ da locadora.
+///    - `numerocontabanco`: Número da conta bancária.
+///    - `numeroagenciabanco`: Número da agência bancária.
+///    - `nomebanco`: Nome do banco.
+///
+/// # Retornos
+/// - Result<serde_json::Value, bool>: Retorna `Ok(locadora)` contendo os dados da locadora em formato JSON.
+///   Retorna `Ok(false)` se houver algum problema na criação do objeto JSON (o que não é esperado neste caso).
+
 #[tauri::command]
 pub fn estrutura_locadora(idendereco: String, cnpj: String, numerocontabanco: String, numeroagenciabanco: String, nomebanco: String) -> Result<serde_json::Value, bool>{
-    let id = controller::enc_senha("cnpj");
+    let id = controller::enc_senha(&cnpj);
     let locadora = serde_json::json!({
         "idendereco": idendereco,
         "idlocadora": id,
+        "cnpj": cnpj,
         "numerocontabanco": numerocontabanco,
         "numeroagenciabanco": numeroagenciabanco,
         "nomebanco": nomebanco,
