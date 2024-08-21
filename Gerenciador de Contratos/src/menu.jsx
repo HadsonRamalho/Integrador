935992997d0cf1bbe32c1334252e2010d80dcadd
HadsonRamalho <hadsonramalho@gmail.com>
2024-08-21 15:06:08 -0300

    Removendo comentário de menu.jsx

--- Gerenciador de Contratos/src/menu.jsx
+++ Gerenciador de Contratos/src/menu.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import CPDF from './pdf_call';
 import './styles.css';
 
-//localStorage.removeItem('token');
+localStorage.removeItem('token');
 
 const rootElement = document.getElementById('pdf');
 if (rootElement) {
