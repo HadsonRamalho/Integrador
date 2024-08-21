41f35293cbf249f0d56ad17678974dc039b157e5
jeffinho1 <jeffersonferreira070@gmail.com>
2024-08-21 08:48:09 -0300

    Model locadora e atualização de senha do usuario

--- Gerenciador de Contratos/src-tauri/src/model/usuario.rs
+++ Gerenciador de Contratos/src-tauri/src/model/usuario.rs
@@ -15,6 +15,20 @@ pub async fn atualiza_email(pool: &Pool, email_antigo: &str, email_novo: &str) -
         }
 
     }
+}
 
+pub async fn atualiza_senha(pool: &Pool, email: &str, senha_nova: &str) -> Result<(), mysql_async::Error>{
+    let mut conn = pool.get_conn().await?;
+    let resultado_conexao = conn.exec_drop("UPDATE usuarios SET senha = :senha_nova WHERE email = :email;", 
+    params!{"senha_nova" => senha_nova, "email" => email}).await;
+    match resultado_conexao{
+        Ok(()) => {
+            return Ok(());
+        },
+        Err(e ) => {
+            return Err(e)
 
+        }
+
+    }
 }
