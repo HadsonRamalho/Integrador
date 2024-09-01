c919e4d325ea6db7e19d330f7c031e2cb6833dfa
MauraGusmao <mauragusmaoneta123456@gmail.com>
2024-08-31 22:17:15 -0300

    Removendo arquivos não utilizados

--- Gerenciador de Contratos/src/_arquivo.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Arquivo from "./arquivo";
-import "./styles.css";
-
-  ReactDOM.createRoot(document.getElementById("root")).render(
-      <Arquivo />
-  );  
