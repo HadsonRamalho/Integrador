eb333a6d229da28b8cd0ed6acd57871a30a378d2
jeffinho1 <jeffersonferreira070@gmail.com>
2024-09-08 17:37:35 -0300

    Adicionado busca de maquina por nome e série e busca de locatario por nome e cnpj

--- Gerenciador de Contratos/src-tauri/src/model/locatario.rs
+++ Gerenciador de Contratos/src-tauri/src/model/locatario.rs
@@ -1,7 +1,9 @@
 use mysql_async::prelude::Queryable;
 use crate::model::params;
 use crate::controller;
+use::mysql_async::prelude::FromRow;
 
+#[derive(FromRow)]
 pub struct Locatario{
     pub idlocatario: String,
     pub idendereco: String,
@@ -36,4 +38,36 @@ pub async fn _cadastra_locatario(locatario: Locatario) -> Result<(), mysql_async
         }
     }
     return Ok(());
+}
+
+pub async fn busca_locatario_nome(nome: &str) -> Result<Vec<Locatario>, mysql_async::Error>{
+    let pool = controller::cria_pool().await?;
+    let mut conn = pool.get_conn().await?;
+    let nome_like = format!("%{}%", nome);
+    let locatario = conn.exec("SELECT * FROM locatario WHERE nomelocatario LIKE :nome", params!{"nome" => nome_like}).await;
+    match(locatario){
+        Ok(locatario) =>{
+            println!("Locatarios encontrados");
+            return Ok(locatario);
+        }
+        Err(e) => {
+            println!("{:?}", e);
+            return Err(e);
+        }
+    }
+}
+
+pub async fn busca_locatario_cnpj(cnpj: &str) -> Result<Locatario, mysql_async::Error>{
+    let pool = controller::cria_pool().await?;
+    let mut conn = pool.get_conn().await?;
+    let locatario: Option<Locatario> = conn.exec_first("SELECT * FROM locatario WHERE cnpj = :cnpj", params!{"cnpj" => cnpj}).await?;
+    match(locatario){
+        None => {
+            return Err(mysql_async::Error::Other(Box::new(std::io::Error::new(std::io::ErrorKind::NotFound,
+                "CNPJ não encontrado"))));
+        }
+        Some(locatario) => {
+            return Ok(locatario);
+        }
+    }
 }
--- Gerenciador de Contratos/src-tauri/src/model/maquina.rs
+++ Gerenciador de Contratos/src-tauri/src/model/maquina.rs
@@ -1,9 +1,10 @@
 use mysql_async::{params, prelude::Queryable};
+use mysql_async::prelude::FromRow;
 use serde::Serialize;
 
 use crate::controller;
 
-#[derive(Serialize)]
+#[derive(Serialize, FromRow)]
 pub struct Maquina {
     pub idmaquina: String,
     pub nomemaquina: String,
@@ -40,4 +41,51 @@ pub async fn cadastrar_maquina(maquina: Maquina) -> Result<(), mysql_async::Erro
     }
 
     return Ok(());
+}
+
+pub async fn buscar_maquina_nome(nome: &str) -> Result<Vec<Maquina>, mysql_async::Error>{
+    let pool = match controller::cria_pool().await{
+            Ok(pool) => {
+                pool
+            },
+            Err(e) =>{
+                println!("{:?}", e);
+                return Err(e);
+            }
+    };
+    let mut conn = pool.get_conn().await?;
+    let nome_like = format!("%{}%", nome);
+    let resultado_select = conn.exec("SELECT * FROM maquina WHERE nomemaquina LIKE :nome;", 
+    params!{"nome" => nome_like}).await;
+    match resultado_select{
+        Ok(maquinas) => {
+            println!("Maquinas encontradas");
+            return Ok(maquinas);
+        },
+        Err(e) =>{
+            println!("{:?}", e);
+            return Err(e);
+        }
+
+    }
+}
+
+pub async fn busca_maquina_serie(serie: &str) -> Result<Maquina, mysql_async::Error>{
+    let pool = controller::cria_pool().await?;
+    let mut conn = pool.get_conn().await?;
+    let maquina: Option<Maquina> = conn.exec_first("SELECT * FROM maquina WHERE numserie = :serie", 
+    params! {"serie" => serie}).await?;
+    match maquina{
+        None => {
+            //Criando um erro personalizado para a aplicação.
+            return Err(mysql_async::Error::Other(Box::new(std::io::Error::new(std::io::ErrorKind::NotFound, 
+                "Numero de série não encontrado"))));
+        },
+        Some(maquina) => {
+            return Ok(maquina);
+
+        }
+
+    }
+
 }
