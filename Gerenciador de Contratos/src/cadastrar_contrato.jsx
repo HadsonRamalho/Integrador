abbf2446171d89b93088861f6fa36d92cb224154
HadsonRamalho <hadsonramalho@gmail.com>
2024-09-17 15:06:29 -0300

    Adicionando seletor de UF na interface de Contrato

--- Gerenciador de Contratos/src/cadastrar_contrato.jsx
+++ Gerenciador de Contratos/src/cadastrar_contrato.jsx
@@ -435,11 +435,46 @@ function CadastrarContrato(){
           placeholder="Cidade da Locadora" 
         />
         <br></br>
-        <input required
-          className="rowReset"
-          onChange={(e) => setUfLocadora(e.currentTarget.value)}
-          placeholder="UF da Locadora" 
-        />
+        <div class="input-box">
+                    <label for="estadoLocadora">Estado (UF)</label>
+                    <select id="estadoLocadora" 
+                        name="estadoLocadora" 
+                        value={ufLocadora} // Valor controlado pelo estado
+                        onChange={capturaUfLocadora}
+                        required 
+                        aria-label="Selecione o estado da Locadora"
+                        
+                    >
+                        <option value="" disabled selected>Selecione o estado da Locadora</option>
+                        <option value="AC">AC</option>
+                        <option value="AL">AL</option>
+                        <option value="AP">AP</option>
+                        <option value="AM">AM</option>
+                        <option value="BA">BA</option>
+                        <option value="CE">CE</option>
+                        <option value="ES">ES</option>
+                        <option value="GO">GO</option>
+                        <option value="MA">MA</option>
+                        <option value="MT">MT</option>
+                        <option value="MS">MS</option>
+                        <option value="MG">MG</option>
+                        <option value="PA">PA</option>
+                        <option value="PB">PB</option>
+                        <option value="PR">PR</option>
+                        <option value="PE">PE</option>
+                        <option value="PI">PI</option>
+                        <option value="RJ">RJ</option>
+                        <option value="RN">RN</option>
+                        <option value="RS">RS</option>
+                        <option value="RO">RO</option>
+                        <option value="RR">RR</option>
+                        <option value="SC">SC</option>
+                        <option value="SP">SP</option>
+                        <option value="SE">SE</option>
+                        <option value="TO">TO</option>
+      </select>
+      
+    </div>
         <br></br>
         <input required
           className="rowReset"
