8c7ee513269aa5d1493d575488e028f4a928358d
HadsonRamalho <hadsonramalho@gmail.com>
2024-12-03 16:43:05 -0300

    fix(servidor): trocando porta do servidor pra 3003

--- Gerenciador de Contratos/src-back/src/main.rs
+++ Gerenciador de Contratos/src-back/src/main.rs
@@ -8,6 +8,6 @@ pub mod schema;
 async fn main() {
     tracing_subscriber::fmt::init();
     let app = crate::routes::cria_rotas();
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3003").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
