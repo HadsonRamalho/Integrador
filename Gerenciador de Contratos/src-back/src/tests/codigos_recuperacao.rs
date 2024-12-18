59cf99d9ee3e0fed4bcaa8c6a286d3c9bd2a3bea
HadsonRamalho <hadsonramalho@gmail.com>
2024-12-18 00:07:42 -0300

    test: adicionando mais testes para códigos de recuperação

--- Gerenciador de Contratos/src-back/src/tests/codigos_recuperacao.rs
+++ Gerenciador de Contratos/src-back/src/tests/codigos_recuperacao.rs
@@ -1,6 +1,6 @@
 use axum::Json;
 
-use crate::{controllers::{codigos_recuperacao::{deleta_codigo, envia_codigo_recuperacao}, cria_conn, usuarios::{cadastra_usuario, estrutura_usuario, realiza_login, CredenciaisUsuario, EmailInput}}, models::{codigos_recuperacao::cadastra_codigo_recuperacao_db, usuarios::deleta_usuario}, tests::usuarios::usuario_padrao};
+use crate::{controllers::{codigos_recuperacao::{deleta_codigo, envia_codigo_recuperacao, verifica_codigo_recuperacao, CodigoRecuperacaoInput}, cria_conn, usuarios::{cadastra_usuario, estrutura_usuario, realiza_login, CredenciaisUsuario, EmailInput}}, models::{codigos_recuperacao::cadastra_codigo_recuperacao_db, usuarios::deleta_usuario}, tests::usuarios::usuario_padrao};
 
 #[tokio::test]
 async fn test_cadastra_codigo_ok(){
@@ -13,6 +13,43 @@ async fn test_cadastra_codigo_ok(){
     assert!(cadastra_usuario(usuario).await.is_ok());
 
     let idcodigo = envia_codigo_recuperacao(Json(EmailInput{email})).await.unwrap().1.0;
+
+    assert!(deleta_codigo(idcodigo).await.is_ok());
+    assert!(deleta_usuario(id).await.is_ok());
+}
+
+#[tokio::test]
+async fn test_cadastra_codigo_err(){
+    let usuario = usuario_padrao("101");
+
+    let usuario = estrutura_usuario(Json(usuario)).await.unwrap().1;
+    let id = usuario.idusuario.clone();
+
+    assert!(cadastra_usuario(usuario).await.is_ok());
+
+    let email_invalido = "emailinvalido@gmail.com".to_string();
+
+    assert!(envia_codigo_recuperacao(Json(EmailInput{email: email_invalido})).await.is_err());
+    
+    assert!(deleta_usuario(id).await.is_ok());
+}
+
+#[tokio::test]
+async fn test_verifica_codigo_err(){
+    let usuario = usuario_padrao("102");
+    let email = usuario.email.to_string();
+
+    let usuario = estrutura_usuario(Json(usuario)).await.unwrap().1;
+    let id = usuario.idusuario.clone();
+
+    assert!(cadastra_usuario(usuario).await.is_ok());
+
+    let idcodigo = envia_codigo_recuperacao(Json(EmailInput{email})).await.unwrap().1.0;
+
+    assert!(verifica_codigo_recuperacao(Json(CodigoRecuperacaoInput{
+        idusuario: id.clone(),
+        codigodigitado: "CodInvalido".to_string()
+    })).await.is_err());
 
     assert!(deleta_codigo(idcodigo).await.is_ok());
     assert!(deleta_usuario(id).await.is_ok());
