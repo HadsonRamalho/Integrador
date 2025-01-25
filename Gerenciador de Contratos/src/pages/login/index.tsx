a715eaa401ef508c8321ecef1d2fb1fc197622f9
HadsonRamalho <hadsonramalho@gmail.com>
2025-01-25 15:06:58 -0300

    feat(front): alterando estilos css

--- Gerenciador de Contratos/src/pages/login/index.tsx
+++ Gerenciador de Contratos/src/pages/login/index.tsx
@@ -187,7 +187,7 @@ export default function AuthPage() {
               )}
             </div>
           </div>
-          <div>
+          <div className="imagem-login">
             <img src={maquina} alt="" />
           </div>
         </div>
