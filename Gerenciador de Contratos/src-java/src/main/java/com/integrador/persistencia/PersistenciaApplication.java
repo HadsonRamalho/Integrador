224a305d71bc2b2f066afd5b2513e9af343e4b4f
jeffinho1 <jeffersonferreira070@gmail.com>
2025-02-08 22:28:49 -0300

    Adicionando src-java e proto

---
+++ Gerenciador de Contratos/src-java/src/main/java/com/integrador/persistencia/PersistenciaApplication.java
@@ -0,0 +1,13 @@
+package com.integrador.persistencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistenciaApplication.class, args);
+	}
+
+}
