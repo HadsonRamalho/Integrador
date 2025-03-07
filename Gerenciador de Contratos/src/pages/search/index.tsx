24d4be713448a87286b9e163d8d3453c806a7da0
MauraGusmao <mauragusmaoneta123456@gmail.com>
2025-03-07 19:32:24 -0300

    feat)front começando a estilizar o resultado- search

--- Gerenciador de Contratos/src/pages/search/index.tsx
+++ Gerenciador de Contratos/src/pages/search/index.tsx
@@ -3,14 +3,15 @@ import { Machine as Maquina } from "@/interfaces/machine";
 import { listMachine } from "@/services/api/machine/machine";
 import { MachineCard } from "@/components/machine-card";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/layouts"; 
+import { Input } from "@/layouts";
 import { useParams } from "react-router-dom";
 import Layout from "@/layouts/default";
 import "@/components/searchs/searchs.css";
+import { Search } from "lucide-react";
 const DetalhesMaquina = () => {
   const [maquinas, setMaquinas] = useState<Maquina[]>([]);
-  const {busca} = useParams();
-  const [filter, setFilter] = useState(busca); 
+  const { busca } = useParams();
+  const [filter, setFilter] = useState(busca);
 
   useEffect(() => {
     const listMachines = async () => {
@@ -21,50 +22,49 @@ const DetalhesMaquina = () => {
     listMachines();
   }, []);
 
-
   const filteredMachines = maquinas.filter((maquina) =>
-    maquina.nome.toLowerCase().includes(filter.toLowerCase()) 
+    maquina.nome.toLowerCase().includes(filter.toLowerCase())
   );
 
   return (
     <Layout>
       <main className="max-w-6xl mx-auto p-6">
-      <div className="mt-10 mb-10">
- 
-      <Input
-        type="text"
-        value={filter}
-        onChange={(e) => setFilter(e.target.value)} 
-        placeholder="Buscar máquina..."
-        style={{ marginBottom: "20px", padding: "10px", width: "80%" }}
-      />
+        <div className="mt-10 mb-10 ">
+          <div className="search-busca">
+            <Input
+              type="text"
+              value={filter}
+              onChange={(e) => setFilter(e.target.value)}
+              placeholder="Buscar máquina..."
+              className="search-input-result"
+            />
+            <Search className="search-icon" />
+          </div>
 
- 
-      <div className="flex">
-      {maquinas.length === 0 ? (
-        <div>
-          <p>Houve um erro ao carregar as máquinas. Reporte o problema aqui:</p>
-          <Button>Relatar problema</Button>
+          <div className="machine-grid">
+            {maquinas.length === 0 ? (
+              <div>
+                <p>
+                  Houve um erro ao carregar as máquinas. Reporte o problema
+                  aqui:
+                </p>
+                <Button>Relatar problema</Button>
+              </div>
+            ) : filteredMachines.length > 0 ? (
+              filteredMachines.map((maquina) => (
+                <div
+                  key={maquina.idmaquina}
+                  style={{ marginTop: "4vh", width: "90%", height: "600px" }}
+                >
+                  <MachineCard machine={maquina} />
+                </div>
+              ))
+            ) : (
+              <p>Nenhuma máquina encontrada.</p>
+            )}
+          </div>
         </div>
-      ) : (
-   
-        filteredMachines.length > 0 ? (
-          filteredMachines.map((maquina) => (
-            <div
-              key={maquina.idmaquina}
-              style={{ marginTop: "4vh", width: "90%", height: "600px" }}
-            >
-              <MachineCard machine={maquina} />
-            </div>
-          ))
-        ) : (
-  
-          <p>Nenhuma máquina encontrada.</p>
-        )
-      )}
-      </div>
-    </div>
-    </main>
+      </main>
     </Layout>
   );
 };
