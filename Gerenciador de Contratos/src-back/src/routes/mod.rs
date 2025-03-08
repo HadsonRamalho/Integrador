0001d0423e3fb3d35f4f1fe59d261fc762d4ebc0
HadsonRamalho <hadsonramalho@gmail.com>
2025-03-08 10:01:02 -0300

    feat(back): adicionando rotas de locadora e conta bancária

--- Gerenciador de Contratos/src-back/src/routes/mod.rs
+++ Gerenciador de Contratos/src-back/src/routes/mod.rs
@@ -4,7 +4,7 @@ use axum::{
 use tower_http::cors::{Any, CorsLayer};
 use tower_http::services::ServeDir;
 use utoipa::openapi::Contact;
-use crate::controllers::{codigos_recuperacao::{envia_codigo_recuperacao, verifica_codigo_recuperacao}, enderecos::{atualiza_endereco, busca_endereco_id, busca_endereco_idusuario, cadastra_endereco_usuario}, imagens_maquinas::{cadastra_imagem_maquina, recupera_imagem_maquina, recupera_imagens_maquina}, locatarios::{busca_locatario_idusuario, cadastra_locatario}, maquinas::{atualiza_maquina, busca_maquina_idpublico, cadastra_maquina, lista_todas_maquinas, pesquisa_maquina}, maquinas_usuarios::busca_maquinas_usuario_idusuario, multipart::cadastra_imagem, oauth::{google_oauth_handler, Config}, usuarios::{self, atualiza_email_usuario, atualiza_senha_usuario, atualiza_usuario, busca_email_usuario, busca_usuario_documento, busca_usuario_id, cadastra_usuario, deleta_usuario, realiza_login, redefine_senha_usuario}};
+use crate::controllers::{codigos_recuperacao::{envia_codigo_recuperacao, verifica_codigo_recuperacao}, contas_bancarias::{busca_conta_bancaria_idusuario, cadastra_conta_bancaria}, enderecos::{atualiza_endereco, busca_endereco_id, busca_endereco_idusuario, cadastra_endereco_usuario}, imagens_maquinas::{cadastra_imagem_maquina, recupera_imagem_maquina, recupera_imagens_maquina}, locadoras::{busca_locadora_idusuario, cadastra_locadora}, locatarios::{busca_locatario_idusuario, cadastra_locatario}, maquinas::{atualiza_maquina, busca_maquina_idpublico, cadastra_maquina, lista_todas_maquinas, pesquisa_maquina}, maquinas_usuarios::busca_maquinas_usuario_idusuario, multipart::cadastra_imagem, oauth::{google_oauth_handler, Config}, usuarios::{self, atualiza_email_usuario, atualiza_senha_usuario, atualiza_usuario, busca_email_usuario, busca_usuario_documento, busca_usuario_id, cadastra_usuario, deleta_usuario, realiza_login, redefine_senha_usuario}};
 use crate::controllers::usuarios::busca_usuario_email;
 use crate::routes::usuarios::{__path_realiza_login, __path_cadastra_usuario, __path_busca_usuario_email, __path_atualiza_usuario, __path_atualiza_senha_usuario, __path_redefine_senha_usuario, __path_busca_email_usuario, __path_atualiza_email_usuario, __path_busca_usuario_id};
 use crate::controllers::codigos_recuperacao::__path_envia_codigo_recuperacao;
@@ -87,6 +87,12 @@ pub fn cria_rotas() -> Router<>{
 
         .route("/cadastra_locatario", post(cadastra_locatario))
         .route("/busca_locatario_idusuario/", get(busca_locatario_idusuario))
+
+        .route("/cadastra_locadora", post(cadastra_locadora))
+        .route("/busca_locadora_idusuario/", get(busca_locadora_idusuario))
+
+        .route("/cadastra_conta_bancaria", post(cadastra_conta_bancaria))
+        .route("/busca_conta_bancaria_idusuario/", get(busca_conta_bancaria_idusuario))
 
         .route("/busca_maquinas_usuario_idusuario/", get(busca_maquinas_usuario_idusuario))
 
