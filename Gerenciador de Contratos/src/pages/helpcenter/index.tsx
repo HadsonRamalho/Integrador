0db0393cc84940f7c3d88191023e902bbeb4b480
MauraGusmao <mauragusmaoneta123456@gmail.com>
2025-03-08 18:14:19 -0300

    feat(front) estilizando helpcenter

--- Gerenciador de Contratos/src/pages/helpcenter/index.tsx
+++ Gerenciador de Contratos/src/pages/helpcenter/index.tsx
@@ -1,30 +1,56 @@
 import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/layouts";
 import { Search } from "lucide-react";
 import { useState } from "react";
+import "@/components/helpcenter/helpcenter.css"
 
 export default function HelpCenter() {
       const [search, setSearch] = useState("");
+
+const duvidas =[
+  {id: 1, categoria :"FAQ"},
+  {id: 2, categoria: "Tutorial"},
+  {id: 3, categoria: "Suporte"},
+]
+  const filteredDoubts = duvidas.filter((duvidas) =>
+    duvidas.categoria.toLowerCase().includes(search.toLowerCase())
+  );
     return (
-        <>
+
         <div>
       <h1 >Central de Ajuda</h1>
-      <div >
-        <Search  />
+      <div  className="search-busca mt-10 ml-10 mr-10">
+        <Search className="search-icon" />
         <Input
           type="text"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
           placeholder="Pesquise por uma dúvida..."
+          className="search-input"
           
         />
       </div>
-        <p >Nenhum resultado encontrado.</p>
-      <div >
+      
+      <div>
+
+       {filteredDoubts.length > 0 ? (
+        filteredDoubts.map((duvidas) => (
+          <Card key={duvidas.id} className="">
+            <CardContent>
+              <h2 className="text-black">{duvidas.categoria}</h2>
+            
+            </CardContent>
+          </Card>
+        ))
+      ) : (
+        <p className="">Nenhum resultado encontrado.</p>
+      )}
         <Button >Falar com o Suporte</Button>
       </div>
-    </div>
-        </>
+      </div>
+   
+  
     );
 
 }
