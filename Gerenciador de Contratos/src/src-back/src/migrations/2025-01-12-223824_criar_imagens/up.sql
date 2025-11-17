23249eac56a3cefd5203ca5b338cdabe5ad1ab90
HadsonRamalho <hadsonramalho@gmail.com>
2025-11-17 19:58:33 -0300

    fix: delete src-back from frontend src

--- Gerenciador de Contratos/src/src-back/src/migrations/2025-01-12-223824_criar_imagens/up.sql
+++
@@ -1,7 +0,0 @@
--- Your SQL goes here
-CREATE TABLE imagens(
-    idimagem VARCHAR(64) PRIMARY KEY,
-    nome VARCHAR(128) NOT NULL,
-    bin BYTEA NOT NULL,
-    link VARCHAR(128) NOT NULL
-)
--- Gerenciador de Contratos/src/src-back/src/migrations/2025-01-24-231231_alterar_usuarios_adicionar_origemconta/up.sql
+++
@@ -1,2 +0,0 @@
--- Your SQL goes here
-ALTER TABLE usuarios ADD COLUMN origemconta VARCHAR(64) NOT NULL;
